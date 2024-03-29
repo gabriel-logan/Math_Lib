b4eac5315d1508e99e50e360ff8a84529b7fa949
gabriel-logan <gabrielcoutolima@protonmail.ch>
2024-03-29 10:28:51 -0300

    feat: adding prettier

--- packages/typescript/.eslintrc.cjs
+++ packages/typescript/.eslintrc.cjs
@@ -1,11 +1,12 @@
 /* eslint-env node */
 module.exports = {
-    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-    parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint'],
-    root: true,
-    rules: {
-        semi: ['error', 'always'], // Enforce semicolons
-        quotes: ['error', 'single'], // Enforce single quotes for strings
-    },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  root: true,
+  rules: {},
 };
